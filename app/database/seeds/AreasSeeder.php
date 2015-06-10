0e0afca53a6a30981a19c0f6a378b6c9d00508a5
RuizAngie <angelica.ruiz921@gmail.com>
2015-06-10 10:17:23 -0500

    descripcion areas y cargos

--- app/database/seeds/AreasSeeder.php
+++ app/database/seeds/AreasSeeder.php
@@ -1,4 +1,4 @@
-﻿<?php 
+<?php 
 	/**
 	* 
 	*/
@@ -7,7 +7,17 @@
     	public function run(){
     		
 	        Area::create(array('IdArea' => '1','NombreArea' => 'Dirección','DescripcionArea' =>'Dirección del CMPL'));
-	        	        
+	        Area::create(array('IdArea' => '2','NombreArea' => 'Subdirección Técnica','DescripcionArea' => 'Realizar proyectos de producción mas límpia y eficiencia energética'));
+	        Area::create(array('IdArea' => '3','NombreArea' => 'Subdirección de Posgrado','DescripcionArea' => 'Formar recursos humanos en producción más límpia')); 	        
+	        Area::create(array('IdArea' => '4','NombreArea' => 'Subdirección de Vinculación y Apoyo','DescripcionArea' => 'Ofertar los servicios del CMPL' ));
+	        Area::create(array('IdArea' => '5','NombreArea' => 'Departamento de Ingeniería de Procesos','DescripcionArea' => 'Ayudar a empresas a reducir residuos'));
+	        Area::create(array('IdArea' => '6','NombreArea' => 'Departamento de Ingeniería de Energía','DescripcionArea' => 'Ayudar a empresas a reducir residuos'));
+	        Area::create(array('IdArea' => '7','NombreArea' => 'Departamento de Servicios Administrativos y Técnicos','DescripcionArea' => 'Administrar los recurso asignados al CMPL'));
+	        Area::create(array('IdArea' => '8','NombreArea' => 'Departamento de Sistemas y Banco de Datos','DescripcionArea' => 'Administrar los sistemas'));
+	        Area::create(array('IdArea' => '9','NombreArea' => 'Sala de Juntas','DescripcionArea' => 'Reuniones'));
+	        Area::create(array('IdArea' => '10','NombreArea' => 'Biblioteca','DescripcionArea' => 'Biblioteca del CMPL'));
+	        Area::create(array('IdArea' => '11','NombreArea' => 'Vigilancia','DescripcionArea' => 'Policia'));
+
 	        $this->command->info('AreaTableSeeder: Areas cargadas correctamente');
     	}
 	}
--- app/database/seeds/CargosSeeder.php
+++ app/database/seeds/CargosSeeder.php
@@ -6,15 +6,23 @@
 	{
     	public function run(){
     		
-	        Cargo::create(array('IdCargo' => '1','NombreCargo' => '','DescripcionCargo' =>''));
-	        Cargo::create(array('IdCargo' => '2','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '3','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '4','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '5','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '6','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '7','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '8','NombreCargo' => '','DescripcionCargo' =>''));
-			Cargo::create(array('IdCargo' => '9','NombreCargo' => '','DescripcionCargo' =>''));
+	        Cargo::create(array('IdCargo' => '1','NombreCargo' => 'Director','DescripcionCargo' =>'Administrar y transmitir información y tomar decisiones'));
+	        Cargo::create(array('IdCargo' => '2','NombreCargo' => 'Ingeniero de Proyectos ','DescripcionCargo' =>''));
+			Cargo::create(array('IdCargo' => '3','NombreCargo' => 'Asistente (Oficialía de Partes)','DescripcionCargo' =>'Recibir los oficios que entran al CMPL'));
+			Cargo::create(array('IdCargo' => '4','NombreCargo' => 'Subdirector','DescripcionCargo' =>'Apoyar a la dirección en la toma de decisiones'));
+			Cargo::create(array('IdCargo' => '5','NombreCargo' => 'Asistente Subdirección ','DescripcionCargo' =>'Atender asuntos que la Subdirección le asigne'));
+			Cargo::create(array('IdCargo' => '6','NombreCargo' => 'Jefe de Departamento','DescripcionCargo' =>'Encargado de Departamento'));
+			Cargo::create(array('IdCargo' => '7','NombreCargo' => 'Responsable Técnico de Laboratorio','DescripcionCargo' =>''));
+			Cargo::create(array('IdCargo' => '8','NombreCargo' => 'Profesor Investigador ','DescripcionCargo' =>''));
+			Cargo::create(array('IdCargo' => '9','NombreCargo' => 'Control Escolar','DescripcionCargo' =>'Gestionar la información de los alumnos del CMPL'));
+			Cargo::create(array('IdCargo' => '10','NombreCargo' => 'Ingeniero de Vinculación de Proyectos','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '11','NombreCargo' => 'Control y Presupuestos','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '12','NombreCargo' => 'Analista','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '13','NombreCargo' => 'Banco de Datos','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '14','NombreCargo' => 'Contabilidad y Presupuestos','DescripcionCargo'=>'Asigna y gestiona recursos que entran al CMPL'));
+			Cargo::create(array('IdCargo' => '15','NombreCargo' => 'Contabilidad de Vinculados','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '16','NombreCargo' => 'Responsable de Activo Fijo','DescripcionCargo'=>''));
+			Cargo::create(array('IdCargo' => '17','NombreCargo' => 'Capital Humano','DescripcionCargo'=>'Contratación y nomina'));
 	        	        
 	        $this->command->info('CargosTableSeeder: Cargos cargadas correctamente');
     	}
