a9e92c0066abd766fe31fd6e42a25be23f778baa
Oscar <oalcantarac@outlook.com>
2015-06-22 16:31:22 -0500

    Vistas de prueba del CMPL

---
+++ app/controllers/IescmplController.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+class IescmplController extends BaseController {
+
+	public function cmpl_index()
+		{
+			$roles = Rol::all();
+			return View::make('cmpl.cmpl_index');
+		}
+}
+?>
---
+++ app/views/cmpl/cmpl_index.blade.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>Sin título 1</title>
+</head>
+
+<body>
+	<p>Holis!</p>
+</body>
+
+</html>
