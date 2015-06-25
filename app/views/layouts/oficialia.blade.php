dbe3afb379c482c9db2d17630a14320c7e6450e0
Oscar <oalcantarac@outlook.com>
2015-06-25 15:02:24 -0500

    Los layouts están extraños. Intento ver qué pex con los CSS.

--- app/views/layouts/oficialia.blade.php
+++ app/views/layouts/oficialia.blade.php
@@ -11,13 +11,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Font CSS (Via CDN) -->
-  <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'>
   {{HTML::style("http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700")}}
   <!-- Theme CSS -->
-  <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/theme.css">
   {{HTML::style("assets/skin/default_skin/css/theme.css")}}
+  <!-- Admin Forms CSS -->
+  {{HTML::style("assets/admin-tools/admin-forms/css/admin-forms.css")}}
   <!-- Favicon -->
-  <link rel="shortcut icon" href="assets/img/favicon.ico">
   {{HTML::style("assets/img/favicon.ico")}}
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
