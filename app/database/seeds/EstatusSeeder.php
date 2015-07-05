c8bd884643d500d2e20b9b3beead91bb7c6220db
Oscar2991 <oalcantarac@outlook.com>
2015-07-05 02:03:39 -0500

    Cambio en el Seeder de Estatus de correspondencia.

--- app/database/seeds/EstatusSeeder.php
+++ app/database/seeds/EstatusSeeder.php
@@ -6,12 +6,19 @@
 	{
     	public function run(){
     		
-	        Estatus::create(array('NombreEstatus' => 'En revisión'));
-	        Estatus::create(array('NombreEstatus' => 'Enviado'));
+	        Estatus::create(array('NombreEstatus' => 'Registrado'));
+	        Estatus::create(array('NombreEstatus' => 'Turnado'));
+	        Estatus::create(array('NombreEstatus' => 'Enterado'));
+	        Estatus::create(array('NombreEstatus' => 'En atención'));
 	        Estatus::create(array('NombreEstatus' => 'Pendiente'));
-	        Estatus::create(array('NombreEstatus' => 'Cancelado'));
 	        Estatus::create(array('NombreEstatus' => 'Atendido'));
-	        Estatus::create(array('NombreEstatus' => 'En seguimiento'));
+	        Estatus::create(array('NombreEstatus' => 'Captura de respuesta'));
+	        Estatus::create(array('NombreEstatus' => 'En revisión'));
+	        Estatus::create(array('NombreEstatus' => 'Observaciones'));
+	        Estatus::create(array('NombreEstatus' => 'Por firmar'));
+	        Estatus::create(array('NombreEstatus' => 'Enviado'));
+	        Estatus::create(array('NombreEstatus' => 'Acuse pendiente'));
+	        Estatus::create(array('NombreEstatus' => 'Acuse recibido'));
 
 	        $this->command->info('EstatusTableSeeder: Estatus de documentos cargados correctamente');
     	}
